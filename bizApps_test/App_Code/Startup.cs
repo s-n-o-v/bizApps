092314ea79d01edfcea389e046fb44ce0cf41f1b
Sergey Novikov <s-n-o-v@yandex.ru>
2020-02-20 14:26:15 +0300

    Add files via upload

---
+++ bizApps_test/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bizApps_test.Startup))]
+namespace bizApps_test
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
